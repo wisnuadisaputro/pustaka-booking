4e717a593d0c8697d03668406fbbf21f09737d73
wisnuadisaputro <wisnuadisaputro521@gmail.com>
2023-03-21 21:00:49 +0700

    first commit

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,6 +1,5 @@
 <?php
 class Latihan1 extends CI_Controller
-//
 {
  public function index()
  {
@@ -10,8 +9,9 @@ class Latihan1 extends CI_Controller
  public function penjumlahan($n1, $n2)
  {
  $this->load->model('Model_latihan1');
- $hasil = $this->Model_latihan1->jumlah($n1, $n2);
- echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
-.$hasil;
+ $data['nilai1'] = $n1;
+ $data['nilai2'] = $n2;
+ $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+ $this->load->view('view-latihan1', $data);
  }
 }
